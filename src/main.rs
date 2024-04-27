103c17a43b678607395d63f1e0fba38168ceebee
本子画师三岁半 <157886909+spdrwcn@users.noreply.github.com>
2024-04-27 13:34:44 +0800

    1.3.0

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .long("wired")
                 .value_name("Value")
                 .multiple(true)
-                .help("有线网卡匹配参数 [default: gbe true]"),
+                .help("有线网卡匹配参数"),
         )
         .arg(
             Arg::with_name("wireless")
@@ -38,7 +38,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .long("wireless")
                 .value_name("Value")
                 .multiple(true)
-                .help("无线网卡匹配参数 [default: wi-fi true]"),
+                .help("无线网卡匹配参数"),
         )
         .arg(
             Arg::with_name("bluetooth")
@@ -46,26 +46,15 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .long("bluetooth")
                 .value_name("Value")
                 .multiple(true)
-                .help("蓝牙匹配参数 [default: bluetooth true]"),
+                .help("蓝牙匹配参数"),
         )
         .get_matches();
     let ip_address = matches.value_of("ip").unwrap();
+    let wired_params: Vec<&str> = matches.values_of("wired").unwrap_or_default().collect();
+    let wireless_params: Vec<&str> = matches.values_of("wireless").unwrap_or_default().collect();
+    let bluetooth_params: Vec<&str> = matches.values_of("bluetooth").unwrap_or_default().collect();
+
     let serial_number = get_bios_serial_number()?;
-    let wiredk: Vec<&str> = if let Some(values) = matches.values_of("wired") {
-        values.collect()
-    } else {
-        vec!["gbe", "true"]
-    };
-    let wirelessk: Vec<&str> = if let Some(values) = matches.values_of("wireless") {
-        values.collect()
-    } else {
-        vec!["wi-fi", "true"]
-    };
-    let bluetoothk: Vec<&str> = if let Some(values) = matches.values_of("bluetooth") {
-        values.collect()
-    } else {
-        vec!["bluetooth", "true"]
-    };
     let output = Command::new("wmic")
         .args(&[
             "path",
@@ -78,100 +67,164 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .expect("Failed to execute wmic command")
         .stdout
         .unwrap();
+
+    // 根据不同厂家网卡名称定义默认条件组
+    let wired_conditions = vec![
+        vec!["gbe", "true"]
+    ];
+    let wireless_conditions = vec![
+        vec!["wi-fi", "true"],
+        vec!["wireless", "true"],
+        vec!["wi-fi", "ax"],
+    ];
+    let bluetooth_conditions = vec![
+        vec!["bluetooth", "true"]
+    ];
     let reader = io::BufReader::new(output);
     let mut wired_mac = String::new();
     let mut wireless_mac = String::new();
     let mut bluetooth_mac = String::new();
+
     // 获取MAC地址
-    let mut mac_found = false;
     for line in reader.lines() {
-        let line = line.unwrap();
+        let line = line?;
         let line_lower = line.to_lowercase();
-        let contains_all = |keywords: &[&str]| keywords.iter().all(|kw| line_lower.contains(kw));
-        if contains_all(&wiredk) {
+
+        // 检查有线条件
+        if !wired_params.is_empty() {
+            let mut params_matched = vec![false; wired_params.len()];
+            for (index, param) in wired_params.iter().enumerate() {
+                if line_lower.contains(param) {
+                    params_matched[index] = true
+                }
+            }
+            let all_wired_params_matched = params_matched.iter().all(|&matched| matched);
+
+            if all_wired_params_matched && wired_mac.is_empty() {
+                wired_mac = extract_mac_address(&line);
+            }
+        } else if check_conditions(&line_lower, &wired_conditions) {
             if wired_mac.is_empty() {
                 wired_mac = extract_mac_address(&line);
-                mac_found = true;
             }
-        } else if contains_all(&wirelessk) {
+        }
+
+        // 检查无线条件
+        if !wireless_params.is_empty() {
+            let mut params_matched = vec![false; wireless_params.len()];
+            for (index, param) in wireless_params.iter().enumerate() {
+                if line_lower.contains(param) {
+                    params_matched[index] = true
+                }
+            }
+            let all_wireless_params_matched = params_matched.iter().all(|&matched| matched);
+
+            if all_wireless_params_matched && wireless_mac.is_empty() {
+                wireless_mac = extract_mac_address(&line);
+            }
+        } else if check_conditions(&line_lower, &wireless_conditions) {
             if wireless_mac.is_empty() {
                 wireless_mac = extract_mac_address(&line);
-                mac_found |= !mac_found;
             }
-        } else if contains_all(&bluetoothk) {
+        }
+
+        // 检查蓝牙条件
+        if !bluetooth_params.is_empty() {
+            let mut params_matched = vec![false; bluetooth_params.len()];
+            for (index, param) in bluetooth_params.iter().enumerate() {
+                if line_lower.contains(param) {
+                    params_matched[index] = true
+                }
+            }
+            let all_bluetooth_params_matched = params_matched.iter().all(|&matched| matched); 
+
+            if all_bluetooth_params_matched && bluetooth_mac.is_empty() {
+                bluetooth_mac = extract_mac_address(&line);
+            }
+        } else if check_conditions(&line_lower, &bluetooth_conditions) {
             if bluetooth_mac.is_empty() {
                 bluetooth_mac = extract_mac_address(&line);
-                mac_found |= !mac_found;
             }
         }
     }
+
     check_and_assign_if_empty(&mut wired_mac);
     check_and_assign_if_empty(&mut wireless_mac);
     check_and_assign_if_empty(&mut bluetooth_mac);
 
     let mut redis_ok = String::new();
     let mut redis_error = String::new();
-    // redis写入MAC地址
-    if mac_found {
-        let json_data = json!({
-            "wired_mac": wired_mac,
-            "wireless_mac": wireless_mac,
-            "bluetooth_mac": bluetooth_mac
-        });
-        let json_str = json_data.to_string();
-        let mac_qr: String = format!("序列号:{}\n有线MAC地址:{}\n无线MAC地址:{}\n蓝牙MAC地址:{}",
-            serial_number, wired_mac, wireless_mac, bluetooth_mac
-        );
-        if let Ok(mut client) = simple_redis::create(ip_address) {
-            let set_result = client.set(&serial_number, &*json_str);
-            if set_result.is_ok() {
-                redis_ok = format!("MAC地址: 写入成功");
-            } else {
-                redis_error = format!("MAC地址: 写入失败");
-            }
-            let quit_result = client.quit();
-            if quit_result.is_ok() {
-            } else {
-                redis_error = format!("Error: {}", quit_result.err().unwrap());
-            }
+    // redis写入MAC
+    let json_data = json!({
+        "wired_mac": wired_mac,
+        "wireless_mac": wireless_mac,
+        "bluetooth_mac": bluetooth_mac
+    });
+    let json_str = json_data.to_string();
+    let mac_qr: String = format!(
+        "{} {} {} {}",
+        serial_number, wired_mac, wireless_mac, bluetooth_mac
+    );
+    if let Ok(mut client) = simple_redis::create(ip_address) {
+        let set_result = client.set(&serial_number, &*json_str);
+        if set_result.is_ok() {
+            redis_ok = format!("MAC地址: 写入成功");
         } else {
-            redis_error = format!("Redis 服务端: 连接失败");
+            redis_error = format!("MAC地址: 写入失败");
         }
-        let options = eframe::NativeOptions {
-            viewport: egui::ViewportBuilder::default().with_inner_size([450.0, 650.0]),
-            ..Default::default()
-        };
-        let _ =
-            eframe::run_simple_native("MAC地址采集客户端", options, move |ctx, _frame| {
-                setup_custom_fonts(ctx);
-                egui::CentralPanel::default().show(ctx, |ui| {
-                    ui.label("SN-MAC地址二维码");
-                    let img = ui.ctx().load_texture(
-                        "qr_code",
-                        generate_qrcode_imagedata(&mac_qr),
-                        Default::default(),
-                    );
-                    ui.add(egui::Image::new(&img));
-                    ui.heading(format!("序列号: {}", serial_number));
-                    ui.heading(format!("有线MAC地址: {}", wired_mac));
-                    ui.heading(format!("无线MAC地址: {}", wireless_mac));
-                    ui.heading(format!("蓝牙MAC地址: {}", bluetooth_mac));
-                    ui.heading(&redis_ok);
-                    ui.heading(&redis_error);
-                });
-            });
+        let quit_result = client.quit();
+        if quit_result.is_ok() {
+        } else {
+            redis_error = format!("Error: {}", quit_result.err().unwrap());
+        }
+    } else {
+        redis_error = format!("Redis 服务端: 连接失败");
     }
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([300.0, 500.0]),
+        ..Default::default()
+    };
+    let _ = eframe::run_simple_native("MAC地址采集客户端", options, move |ctx, _frame| {
+        setup_custom_fonts(ctx);
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("SN-MAC地址二维码");
+            let img = ui.ctx().load_texture(
+                "qr_code",
+                generate_qrcode_imagedata(&mac_qr),
+                Default::default(),
+            );
+            ui.add(egui::Image::new(&img));
+            ui.heading(format!("序列号: {}", serial_number));
+            ui.heading(format!("有线MAC地址: {}", wired_mac));
+            ui.heading(format!("无线MAC地址: {}", wireless_mac));
+            ui.heading(format!("蓝牙MAC地址: {}", bluetooth_mac));
+            ui.heading(&redis_ok);
+            ui.heading(&redis_error);
+        });
+    });
     Ok(())
 }
+
 //MAC变量检查
 fn check_and_assign_if_empty(s: &mut String) {
     if s.is_empty() {
         *s = "未采集".to_string();
     }
 }
-//MAC地址处理
+
+fn check_conditions(line: &str, conditions: &[Vec<&str>]) -> bool {
+    conditions
+        .iter()
+        .any(|condition_group| condition_group.iter().all(|kw| line.contains(*kw)))
+}
+
 fn extract_mac_address(line: &str) -> String {
-    line.chars().take(17).collect::<String>()
+    let mac_regex = regex::Regex::new(r"([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})").unwrap();
+    mac_regex
+        .captures(line)
+        .and_then(|cap| cap.get(0))
+        .map(|m| m.as_str().to_string())
+        .unwrap_or_default()
 }
 //二维码生成
 fn generate_qrcode_imagedata(content: &str) -> ImageData {
